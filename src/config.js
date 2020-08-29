3cd968d7c0603e942ea68e70df18b33bc1cdb653
zou-uoz <ghyzjyworld_7010@163.com>
2020-08-29 14:05:38 +0800

    pm2 deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-//setupProxy.js文件中直接使用了http://localhost:5000而不是BASE_URL。
 export const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "http://47.115.157.0:5000"
@@ -8,5 +7,5 @@ export const BASE_URL =
 export const IMAGES_DIR =
   process.env.NODE_ENV === "production"
     ? "http://47.115.157.0/images"
-    : "http://47.115.157.0:5000/images/uploads/";
+    : "http://localhost:5000/images/uploads/";
 // export const IMAGES_DIR = "http://47.115.157.0:5000/images/uploads/";
