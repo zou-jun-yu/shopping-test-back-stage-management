a6e2a16b12a25ceb24cc75a0008ae8a7aab995b8
zou-uoz <ghyzjyworld_7010@163.com>
2020-09-07 17:29:32 +0800

    化简了一些变量名称，useReducer和useContext hooks保存祖先链改为用redux

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,9 +21,10 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:zou-uoz/shopping-back-stage-management.git",
       path: "/root/shopping/shopping-back-stage-management",
-      // "post-deploy": "git pull&&npm install&&npm run build&&cp -rf /root/shopping/shopping-back-stage-management/current/build/* /usr/share/nginx/html/back-manage/",
       "post-deploy":
-        "git pull&&npm run build&&cp -rf /root/shopping/shopping-back-stage-management/current/build/* /usr/share/nginx/html/back-manage/",
+        "git pull&&npm install&&npm run build&&cp -rf /root/shopping/shopping-back-stage-management/current/build/* /usr/share/nginx/html/back-manage/",
+      // "post-deploy":
+      //   "git pull&&npm run build&&cp -rf /root/shopping/shopping-back-stage-management/current/build/* /usr/share/nginx/html/back-manage/",
       env: {
         NODE_ENV: "production",
       },
